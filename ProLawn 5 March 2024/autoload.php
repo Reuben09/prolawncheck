0a221a51ff1ba5a460c7561078955c579597b60c
Chukwuka Reuben <chukwukareuben09@gmail.com>
2024-03-05 22:24:49 +0000

    Delete ProLawn 5 March 2024 directory

--- ProLawn 5 March 2024/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-function autoload ($pClassName) {
-    include(__DIR__ . "/module/" . $pClassName . ".php");
-}
-
-spl_autoload_register("autoload");
--- ProLawn 5 March 2024/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
- $con=mysqli_connect('wayne90103.ipagemysql.com', 'wayne', 'annie10','ss_dbname_castt1') or die("ERROR: Could not connect. " . mysqli_connect_error());
-
-//echo "Database Connected - conn.php ";
-//echo str_repeat("&nbsp;", 60); 
-
-error_reporting(E_ALL);
-?>
